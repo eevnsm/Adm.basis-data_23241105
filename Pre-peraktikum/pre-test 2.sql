293dc0ee1c2e8f33dc4bea256917ccb3ec52d79f
eevnsm <147676383+eevnsm@users.noreply.github.com>
2025-05-21 08:53:30 +0800

    Add files via upload

---
+++ Pre-peraktikum/pre-test 2.sql
@@ -0,0 +1,11 @@
+-- nama : Angga Saputra
+-- NIM : 23241084
+-- kelas : C
+
+-- Melihat database yang ada
+show DATABASES ;
+
+-- melihat database
+USE mysql;
+-- melihat tabel dalam database mysql
+show TABLES
