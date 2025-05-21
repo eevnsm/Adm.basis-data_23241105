56bf6891749cf22169920b4f3fa34bd515bca806
eevnsm <147676383+eevnsm@users.noreply.github.com>
2025-05-21 09:00:46 +0800

    pre-test 2.sql

--- Pre-peraktikum/pre-test 2.sql
+++ Pre-peraktikum/pre-test 2.sql
@@ -9,3 +9,8 @@ show DATABASES ;
 USE mysql;
 -- melihat tabel dalam database mysql
 show TABLES
+
+--Mendeskripsikan sebuah table yang di pilih ex. tabel 'user'
+DESCRIBEE user;
+--Melihat isi tabel user, khusu untuk kolom host, user dam password
+select host, user, password FROM user
