b76d7781762d9236a7b2d734ead042e4e506687c
eevnsm <147676383+eevnsm@users.noreply.github.com>
2025-07-16 10:00:01 +0800

    SOAL UAS.sql

--- UAS/SOAL UAS.sql
+++ UAS/SOAL UAS.sql
@@ -1,9 +1,9 @@
--- nama: Angga Saputra
--- nim : 23241084
+-- nama: Sami Sayyiddin
+-- nim : 23241105
 -- kelas: pti/c 
 
 -- soal 1
-/*munculkan kode pelanggan, nama produk, kuantitinya harga dan total dari semua produk 
+munculkan kode pelanggan, nama produk, kuantitinya harga dan total dari semua produk 
 yang pernah transaksinya, namun output yang diminta 200.000 dan di urutkan berdasarkan total harga 
 terkecil
 
@@ -15,7 +15,7 @@ munculkan kode pelanggan, nama pelanggan, alamat, dan nilai transaksi dari pelan
 transaksinya.
 
 
-jawab soal 1*/
+jawab soal 1
 select tp.kode_pelanggan, tp.nama_produk, tp.qty, tp.harga, (tp.qty * tp.harga) as total_harga
 from tr_penjualan as tp
 where (tp.qty * tp.harga) >= 200000
@@ -24,4 +24,3 @@ order by total_harga asc;
 select tp.kode_pelanggan, mp.nama_pelanggan, mp.alamat, max(tp.qty * tp.harga) as total_harga from tr_penjualan as tp join ms_pelanggan as mp on tp.kode_pelanggan = mp.kode_pelanggan group by tp.kode_pelanggan
 order by total_harga desc;
 
-select nama_pelanggan from ms_pelanggan;
